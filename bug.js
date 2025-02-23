a7fef1639aa5e6c074e130da26612528dd362728
BugHunter-X <askzenai@gmail.com>
2025-02-23 22:36:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//Error: Unhandled exception error
